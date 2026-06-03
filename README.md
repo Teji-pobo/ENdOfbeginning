73e6c61160acdb25eb753b0acec001255a44c8ae
Teji-pobo <tejipobo762@gmail.com>
2026-06-03 15:13:13 +0530

    i am new

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 hee hee i do cp
 <br>
 lets see how it works
+can ud imody
---
+++ hello.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+
+void solve() {
+    
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
